7da0da082cf2f4b88fc9ef9c64a5fd1a75b7817c
nathaneastwood <nathan.eastwood@icloud.com>
2021-06-01 18:46:38 +0200

    Initial Commit

---
+++ Memorise/MemoriseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriseApp.swift
+//  Memorise
+//
+//  Created by Nathan Eastwood on 01/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct MemoriseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
